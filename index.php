4276a1723d18ff6ad8dfc880de3b34525b4a56aa
Isaque Costa - (block) <isaquecostaa@gmail.com>
2023-01-23 07:30:15 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<html lang="pt-BR">
+
+<head>
+    <!-- META DEFAULT -->
+    <meta name="robots" content="index, follow" />
+    <meta name="author" content="IdeYou - Isaque Costa" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- PWA -->
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#ff5356">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Links IdeYou">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
+
+    <!-- META CLIENT -->
+    <title>Links IdeYou</title>
+    <meta content="Links IdeYou" property="og:title">
+    <meta name="keywords" content="IdeYou, agência, tecnologia, marketing, marketingdigital, marketing, o, empreendedorismo, digitalmarketing, instagram, socialmedia, business, sucesso, branding, like, digital, socialmediamarketing, negocios, love, hotmart, empreender, dise, a, marketingonline, follow, marketingstrategy, brasil, emprendedores, advertising, rendaextra, design, dinheiro, photography, bhfyp, art, fashion, bhfyp, marketingdigitalbrasil, empreendedor, foco, vendasonline, mktdigital, seo, marketingdeconteudo, redessociales, motiva, m, vendas, entrepreneur, content, copywriting, emprendimiento, publicidad, onlinemarketing, instagood, tiktok, publicidade, copywriter, contentmarketing, print, yourself, original, copywritingtips, c">
+    <meta name="description" content="Desenvolvido por IdeYou - Acelerando Ideias!" />
+    <meta content="Desenvolvido por IdeYou - Acelerando Ideias!" property="og:description">
+
+    <!-- DEFAULT FAVICONS -->
+    <link rel="shortcut icon" href="https://cdn.ideyou.com.br/assets/favicons/favicon.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-ipad-retina.png">
+    <link rel="apple-touch-icon" sizes="128x128" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-iphone.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-iphone-retina.png">
+
+    <link rel="apple-touch-startup-image" href="https://cdn.ideyou.com.br/assets/favicons/launch_logo.png">
+
+    <!-- Page CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/signin.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/id_buttons.css" rel="stylesheet" type="text/css">
+    </style>
+</head>
+
+<body class="text-center">
+    <div class="form-signin">
+        <img class="logo" src="https://cdn.ideyou.com.br/assets/favicons/launch_logo.png" alt="IdeYou">
+        <a href="https://api.whatsapp.com/send?phone=5531990712203&text=Oi%2C%20estou%20vindo%20do%20link-tree%20da%20IdeYou%20e%20gostaria%20de%20trocar%20uma%20ideia." class="btn btn-lg btn-block btn-whatsapp">Whatsapp</a>
+        <a href="https://instagram.com/ideyou.com.br" class="btn btn-lg btn-block btn-instagram">Instagram</a>
+        <a href="https://ideyou.com.br" class="btn btn-lg btn-block btn-ideyou">Website</a>
+        <a href="https://linkedin.com/company/ideyou" class="btn btn-lg btn-block btn-linkedin">Linkedin</a>
+    </div>
+    <!-- BEGIN BLOBS -->
+    <img class="blob_red" src="https://cdn.ideyou.com.br/assets/blob_red.png" alt="">
+    <img class="blob_green" src="https://cdn.ideyou.com.br/assets/blob_green.png" alt="">
+    <img class="blob_yellow" src="https://cdn.ideyou.com.br/assets/blob_yellow.png" alt="">
+    <!-- ============================================================== -->
+    <!-- SCRIPTS -->
+    <!-- ============================================================== -->
+    <script type="text/javascript" charset="UTF-8" src="assets/js/_jquery.min.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="assets/js/_popper.min.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="assets/js/_bootstrap.min.js"></script>
+</body>
+
+</html>
