54817b7037c26a573586d701c1504381c5414ab1
Isaque Costa - (block) <isaquecostaa@gmail.com>
2024-02-06 03:53:41 -0300

    icons added and logo updated

--- index.php
+++ index.php
@@ -15,20 +15,19 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
 
     <!-- META CLIENT -->
-    <title>Links IdeYou</title>
     <meta content="Links IdeYou" property="og:title">
     <meta name="keywords" content="IdeYou, agência, tecnologia, marketing, marketingdigital, marketing, o, empreendedorismo, digitalmarketing, instagram, socialmedia, business, sucesso, branding, like, digital, socialmediamarketing, negocios, love, hotmart, empreender, dise, a, marketingonline, follow, marketingstrategy, brasil, emprendedores, advertising, rendaextra, design, dinheiro, photography, bhfyp, art, fashion, bhfyp, marketingdigitalbrasil, empreendedor, foco, vendasonline, mktdigital, seo, marketingdeconteudo, redessociales, motiva, m, vendas, entrepreneur, content, copywriting, emprendimiento, publicidad, onlinemarketing, instagood, tiktok, publicidade, copywriter, contentmarketing, print, yourself, original, copywritingtips, c">
     <meta name="description" content="Desenvolvido por IdeYou - Acelerando Ideias!" />
     <meta content="Desenvolvido por IdeYou - Acelerando Ideias!" property="og:description">
 
     <!-- DEFAULT FAVICONS -->
-    <link rel="shortcut icon" href="https://cdn.ideyou.com.br/assets/favicons/favicon.png">
+    <link rel="shortcut icon" href="https://cdn.ideyou.com.br/assets/favicons/new/favicon.png">
     <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-ipad.png">
     <link rel="apple-touch-icon" sizes="167x167" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-ipad-retina.png">
     <link rel="apple-touch-icon" sizes="128x128" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-iphone.png">
     <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.ideyou.com.br/assets/favicons/touch-icon-iphone-retina.png">
 
-    <link rel="apple-touch-startup-image" href="https://cdn.ideyou.com.br/assets/favicons/launch_logo.png">
+    <link rel="apple-touch-startup-image" href="https://cdn.ideyou.com.br/assets/logos/new/logofull-b.png">
 
     <!-- Page CSS -->
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
@@ -39,11 +38,19 @@
 
 <body class="text-center">
     <div class="form-signin">
-        <img class="logo" src="https://cdn.ideyou.com.br/assets/favicons/launch_logo.png" alt="IdeYou">
-        <a href="https://api.whatsapp.com/send?phone=5531990712203&text=Oi%2C%20estou%20vindo%20do%20link-tree%20da%20IdeYou%20e%20gostaria%20de%20trocar%20uma%20ideia." class="btn btn-lg btn-block btn-whatsapp">Whatsapp</a>
-        <a href="https://instagram.com/ideyou.com.br" class="btn btn-lg btn-block btn-instagram">Instagram</a>
-        <a href="https://ideyou.com.br" class="btn btn-lg btn-block btn-ideyou">Website</a>
-        <a href="https://linkedin.com/company/ideyou" class="btn btn-lg btn-block btn-linkedin">Linkedin</a>
+        <img class="logo" src="https://cdn.ideyou.com.br/assets/logos/new/logofull-b.png" alt="IdeYou">
+        <a href="https://api.whatsapp.com/send?phone=5531990712203&text=Oi%2C%20estou%20vindo%20do%20link-tree%20da%20IdeYou%20e%20gostaria%20de%20trocar%20uma%20ideia." target="_blank" class="btn btn-lg btn-block btn-whatsapp">
+            <i class="mdi mdi-whatsapp m-0"></i> Whatsapp
+        </a>
+        <a href="https://instagram.com/ideyou.com.br/" target="_blank" class="btn btn-lg btn-block btn-instagram">
+            <i class="mdi mdi-instagram m-0"></i> Instagram
+        </a>
+        <a href="https://ideyou.com.br" target="_blank" class="btn btn-lg btn-block btn-ideyou">
+            <i class="mdi mdi-web m-0"></i> Website
+        </a>
+        <a href="https://linkedin.com/company/ideyou" target="_blank" class="btn btn-lg btn-block btn-linkedin">
+            <i class="mdi mdi-linkedin m-0"></i> Linkedin
+        </a>
     </div>
     <!-- BEGIN BLOBS -->
     <img class="blob_red" src="https://cdn.ideyou.com.br/assets/blob_red.png" alt="">
